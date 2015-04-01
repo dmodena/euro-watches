3ea7a1cd667bbac359075ba3d4ee1f731163bdb2
Douglas Modena <d.modena@live.com>
2015-04-01 13:25:41 -0500

    Added some brands and products.

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Product do
-  permit_params :brand, :model, :description, :movement, :case_diameter, :case_material, :strap_material, :swiss_made, :cosc, :geneva_hallmark, :price, :stock_quantity
+  permit_params :brand_id, :model, :description, :movement, :case_diameter, :case_material, :strap_material, :swiss_made, :cosc, :geneva_hallmark, :price, :stock_quantity, :image
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
