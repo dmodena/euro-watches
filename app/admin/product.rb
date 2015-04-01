cf945823e5a33e0d4b081868178c86de0f7369d0
Douglas Modena <d.modena@live.com>
2015-04-01 10:50:59 -0500

    Created controllers for Active Admin.

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Product do
-
+  permit_params :brand, :model, :description, :movement, :case_diameter, :case_material, :strap_material, :swiss_made, :cosc, :geneva_hallmark, :price, :stock_quantity
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
